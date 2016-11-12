77dd91a70784c144fd1e5c670eb05bd50b753f5d
Egidio Santos <1130348@isep.ipp.pt>
2016-11-12 14:03:18 +0000

    Modificacao da aplicacao lugares; Correcao ModelLibrary

---
+++ SmartCity/Lugares/Models/Meteorologia.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace ModelLibrary.Models
+{
+    public class Meteorologia
+    {
+
+        public int MeteorologiaID { get; set; }
+        public int LocalID { get; set; }
+        [DisplayName("Data de Leitura")]
+        public DateTime DataDeLeitura { get; set; }
+        [DisplayName("Hora de Leitura")]
+        public TimeSpan HoraDeLeitura { get; set; }
+        [DisplayName("Temperatura")]
+        public float Temperatura { get; set; }
+        [DisplayName("Vento")]
+        public float Vento { get; set; }
+        [DisplayName("Humidade")]
+        public float Humidade { get; set; }
+        [DisplayName("Pressão")]
+        public float Pressao { get; set; }
+        [DisplayName("NO")]
+        public float NO { get; set; }
+        [DisplayName("NO2")]
+        public float NO2 { get; set; }
+        [DisplayName("CO2")]
+        public float CO2 { get; set; }
+
+        public virtual Local Local { get; set; }
+    }
+}
---
+++ SmartCity/Lugares/Models/POI.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.Serialization;
+
+namespace ModelLibrary.Models
+{
+    public class POI
+    {
+        public int ID { get; set; }
+        public int LocalID { get; set; }
+        [DisplayName("Nome")]
+        public string NomePonto { get; set; }
+        [DisplayName("Descrição")]
+        public string DescricaoPonto { get; set; }
+
+        public virtual Local Local { get; set; }
+    }
+}
